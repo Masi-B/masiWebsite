e4a6f4508c7040b8b3b62cdf6913eb7c8d304e90
Masimba <masimba.banda@gmail.com>
2021-10-23 09:32:04 +1100

    first commit

---
+++ src/components/NavBar/MenuList.js
@@ -0,0 +1,17 @@
+export const MenuList = [
+  {
+    title: "About",
+    url: "#box-container",
+    cName: "nav-links",
+  },
+  {
+    title: "My Projects",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Contact",
+    url: "#",
+    cName: "nav-links",
+  },
+];
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,20 +1,56 @@
+import React, { Component } from 'react';
+import { motion } from "framer-motion";
+import { MenuList } from './MenuList';
+
+
 /*Navigation bar class
 Created 23 Oct 2021
 */
 
-import React, { Component } from 'react'
-
 class NavBar extends Component {
+  state = { clicked: false };
 
-    
-    
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+
     render() {
-        return (
-            <nav>
-              Masimba Banda 
-            </nav>
-        )
-    }
+        return(
+       <nav className="navItems">
+         <motion.h1
+           className="navLogo"
+           animate={{
+             scale: [1, 1.2, 1.2, 1.1, 1],
+             borderRadius: ["10%", "10%", "25%", "25%", "10%"],
+             // rotate: [0, 0, 270, 270, 0],
+           }}
+           transition={{
+             duration: 3,
+             repeat: Infinity,
+           }}
+         >
+           Masimba Banda
+           <i className="masi"></i>
+         </motion.h1>
+         <div className="menuIcon" onClick={this.handleClick}>
+           <i
+             className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+           ></i>
+         </div>
+         <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+           {MenuList.map((item, index) => {
+             return (
+               <li key={index}>
+                 <a className={item.cName} href={item.url}>
+                   {item.title}
+                 </a>
+               </li>
+             );
+           })}
+         </ul>
+       </nav>
+           );
+  }
 }
 
 export default NavBar
