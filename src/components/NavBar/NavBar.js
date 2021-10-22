b23211ccff6900446772126a77e09420d9e298d4
Masimba <masimba.banda@gmail.com>
2021-10-23 09:04:39 +1100

    first commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+/*Navigation bar class
+Created 23 Oct 2021
+*/
+
+import React, { Component } from 'react'
+
+class NavBar extends Component {
+
+    
+    
+    render() {
+        return (
+            <nav>
+              Masimba Banda 
+            </nav>
+        )
+    }
+}
+
+export default NavBar
